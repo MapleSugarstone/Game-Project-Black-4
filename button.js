9a9e77951aa8781c4a6d187af6fd4efecc6f5409
MapleSugarstone <147115874+MapleSugarstone@users.noreply.github.com>
2025-01-22 13:15:06 -0800

    Bugged Button Method

--- button.js
+++ button.js
@@ -1,28 +1,35 @@
 class Button {
-	constructor(x, y, sprite, width, height) {
+	constructor(x, y, sprite, width, height, hoversprite) {
 
 		this.x = x;
 		this.y = y;
 		this.sprite = sprite;
 		this.width = width;
 		this.height = height;
+		this.hoversprite = hoversprite;
+		this.truesprite = sprite;
 
 	};
 
 
 	update() {
-		if ((gameEngine.mouse?.x > this.x & gameEngine.mouse?.x < this.x + this.width ) & (gameEngine.mouse?.y < this.y+this.height & gameEngine.mouse?.y > this.y)) {
-			this.sprite = "./startButton2.png";
-			console.log(2);
-			
-		} else {
-			this.sprite = "./startButton.png";
-		}
+		hovering = checkOverlap(gameEngine.mouse?.x, gameEngine.mouse?.y);
 
 
 	};
 
 	draw(ctx) {
-		ctx.drawImage(ASSET_MANAGER.getAsset(this.sprite), this.x, this.y)
+		ctx.drawImage(ASSET_MANAGER.getAsset(this.truesprite), this.x, this.y)
 	};
+
+	
+	
+
+	checkOverlap(x, y) {
+		if ((x > this.x & x < this.x + this.width ) & (y < this.y+this.height & y > this.y)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- main.js
+++ main.js
@@ -10,8 +10,8 @@ ASSET_MANAGER.queueDownload("./startButton2.png")
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
-	gameEngine.addEntity(new Button(650, 700, "./startButton.png", 672, 131))
-	gameEngine.addEntity(new Background(0, 0, "./Menu.png"))
+	gameEngine.addEntity(new Button(650, 700, "./startButton.png", 672, 131, "./startButton2.png"));
+	gameEngine.addEntity(new Background(0, 0, "./Menu.png"));
 
 	//gameEngine.addEntity(new Unit(gameEngine, 0, 0, "./testUnit.png"))
 
